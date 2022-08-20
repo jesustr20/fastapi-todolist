de0959cf2ff3ed41d748cc9d6248b401aa09df58
jesustr20 <jesus.tr61@gmail.com>
2022-08-20 01:26:10 -0500

    add update, delete

--- app/v1/router/user_router.py
+++ app/v1/router/user_router.py
@@ -1,15 +1,20 @@
 from fastapi import APIRouter, Depends, status, Body
+from fastapi.security import OAuth2PasswordRequestForm
 
 from app.v1.schema import user_schema
 from app.v1.service import user_service
+from app.v1.service import auth_service
+from app.v1.schema.token_schema import Token
 
 from app.v1.utils.db import get_db
 
-router = APIRouter(prefix="/api/v1")
+router = APIRouter(
+  prefix="/api/v1",
+  tags=["user"]
+  )
 
 @router.post(
   "/user/",
-  tags=["users"],
   status_code=status.HTTP_201_CREATED,
   response_model=user_schema.User,
   dependencies=[Depends(get_db)],
@@ -29,3 +34,23 @@ def create_user(user:user_schema.UserRegister=Body(...)):
   - user: User info
   """
   return user_service.create_user(user)
+
+@router.post(
+  "/login",
+  tags=["user"],
+  response_model=Token,
+)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+  """
+  ### Login for access token
+  
+  ### Args
+  the app can recive next fields by form data
+  - username: Your username or email
+  - password: Your password
+  
+  ### Returns
+  - access token and Type
+  """
+  access_token = auth_service.generate_token(form_data.username, form_data.password)
+  return Token(access_token=access_token, token_type="bearer")
