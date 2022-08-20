104c933b9d7f28b2d37235befefc44b99fe102ca
jesustr20 <jesus.tr61@gmail.com>
2022-08-20 03:13:42 -0500

    test

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -31,3 +31,25 @@ def create_user_and_make_login(username: str):
 
   data = response.json()
   return data['access_token']
+
+def test_create_todo_ok():
+    token = create_user_and_make_login('test_create_todo_ok')
+
+    client = TestClient(app)
+
+    todo = {
+        'title': 'My first task'
+    }
+
+    response = client.post(
+        '/api/v1/to-do/',
+        json=todo,
+        headers={
+            'Authorization': f'Bearer {token}'
+        }
+    )
+
+    assert response.status_code == 201, response.text
+    data = response.json()
+    assert data['title'] == todo['title']
+    assert data['is_done'] == False
